e09c1f5dbaf52b00b4ca6a18b1292e19633246b0
Gekkabijin <a_queen_of_the_night@hotmail.co.jp>
2020-03-08 11:50:29 +0900

    Add: INLINE定義
    コンパイラのバージョン別にINLINE(__inline), FORCEINLINE(__forceinline)を定義

---
+++ system/inline/Inline.hpp
@@ -0,0 +1,20 @@
+﻿/**
+ *  @file  Inline.hpp
+ *  @brief INLINE定義を表します。
+ *
+ *  @author 月下美人
+ */
+#pragma once
+
+
+// マクロ定義
+
+/*! inline展開 */
+#define INLINE __inline
+
+/*! inline強制展開 */
+#ifdef  _MSC_VER
+    #define FORCEINLINE  __forceinline
+#else if __GNUC__
+    #define FORCEINLINE  __attribute__((always_inline)))
+#endif  // _MCS_VER
